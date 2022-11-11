ae460f13a8dc4da34932f8901e9297f2e0cd6ad5
Maou <waspofcrimea@yandex.ru>
2022-11-11 21:45:19 +0300

    add Seminar_1.4

---
+++ Seminar_1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите номер дня:");
+int N = int.Parse(Console.ReadLine()!);
+
+if(N == 1) Console.Write("понедельник");
+
+if(N == 2) Console.Write("вторник");
+if(N == 3) Console.Write("среда");
+if(N == 4) Console.Write("четверг");
+if(N == 5) Console.Write("пятница");
+if(N == 6) Console.Write("суббота");
+if(N == 7) Console.Write("воскресеньье");
