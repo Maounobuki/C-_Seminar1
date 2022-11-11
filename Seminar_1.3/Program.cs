49dcf47d65a5d5a89f2838afb56122af04a9ca2b
Maou <waspofcrimea@yandex.ru>
2022-11-11 21:35:45 +0300

    add Seminar_1.3

---
+++ Seminar_1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число a:");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b:");
+int b = int.Parse(Console.ReadLine()!);
+if(a == b / a)
+{
+    Console.Write("Да");
+
+}
+else 
+{
+    Console.Write("Нет");
+}
