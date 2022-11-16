d0499a2ce747569d205fedc1160242ea77dc003f
Maou <waspofcrimea@yandex.ru>
2022-11-17 00:02:42 +0300

    added seminar2.1

---
+++ Seminar_2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая на вход принимает трёхзначно
+е число и удаляя вторую цифру, выводит двухзначное число*/
+
+//Математический метод
+int number = new Random().Next(100, 1000);
+ Console.WriteLine(number);
+Console.WriteLine($"Число без второй цифры имеет вид: {number/100}{number%10}");
+Console.WriteLine();
+int result = (number/100)*10+number%10;
+Console.WriteLine(result);
+
+//строковый метод
+string num = num1.ToString();
+if(num.Length !=3){
+    Console.WriteLine("Неверное число!");
+}
+else
+{
+    Console.WriteLine(num[0], num[2]);
+}
